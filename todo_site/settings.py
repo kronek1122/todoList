e7b8b1d37a1cb3a360c752e72914e7754fa421f7
Daniel <kronkowski.daniel@gmail.com>
2022-11-28 19:57:17 +0100

    sk change

--- todo_site/settings.py
+++ todo_site/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qnjx$_gwmjk8f#his4t$=13vi3ijdi&kvu95g8nsamlsg*ljys'
-
+from decouple import config
+SECRET_KEY = config("SECRET_KEY") # this is to replace the secret key you cut away before
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
